98cfa8e71d7674698e395ad8402620c39bb03e84
michaelshiloh <michael@michaelshiloh.com>
2016-09-22 15:16:30 -0700

    Update arduinoReceive.ino
    use the right variables when moving map() and constrain() here

--- arduinoReceive/arduinoReceive.ino
+++ arduinoReceive/arduinoReceive.ino
@@ -15,13 +15,13 @@ void loop() {
 
     // Map the potentiometer values to those 
     // suitable for analogWrite()
-    pot0 = map(pot0, 0, 1023, 0, 255);
-    pot1 = map(pot1, 0, 1023, 0, 255);
+    redValue = map(redValue, 0, 1023, 0, 255);
+    greenValue = map(greenValue, 0, 1023, 0, 255);
 
     // Constrain the values, just in case 
     // something was outside the expected range
-    pot0 = constrain(pot0, 0, 255);
-    pot1 = constrain(pot1, 0, 255);
+    redValue = constrain(redValue, 0, 255);
+    greenValue = constrain(greenValue, 0, 255);
 
     analogWrite(redLedPin, redValue);
     analogWrite(greenLedPin, greenValue);
