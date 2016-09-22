2b314285a54113760b48053ebd4194a510f13ca7
michaelshiloh <michael@michaelshiloh.com>
2016-09-22 15:10:50 -0700

    Update arduinoReceive.ino
    moved map() and constrain() to receiver

--- arduinoReceive/arduinoReceive.ino
+++ arduinoReceive/arduinoReceive.ino
@@ -1,8 +1,29 @@
+const int greenLedPin = 11;
+const int redLedPin = 10;
+
+void setup() {
+  Serial.begin(9600);
+}
+
 void loop() {
-  // put your main code here, to run repeatedly:
+  int redValue;
+  int greenValue;
+
   if (Serial.available() > 0) {
-   int red = Serial.parseInt();
-   int green = Serial.parseInt();
-   int blue = Serial.parseInt();
+    redValue = Serial.parseInt();
+    greenValue = Serial.parseInt();
+
+    // Map the potentiometer values to those 
+    // suitable for analogWrite()
+    pot0 = map(pot0, 0, 1023, 0, 255);
+    pot1 = map(pot1, 0, 1023, 0, 255);
+
+    // Constrain the values, just in case 
+    // something was outside the expected range
+    pot0 = constrain(pot0, 0, 255);
+    pot1 = constrain(pot1, 0, 255);
+
+    analogWrite(redLedPin, redValue);
+    analogWrite(greenLedPin, greenValue);
   }
 }
